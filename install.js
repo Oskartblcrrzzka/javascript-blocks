024349d4956cbd9b962221ff09af7f787e9b2b38
Oskartblcrrzzka <00799c85@opayq.com>
2016-06-02 20:16:38 +0200

    Update install.js

--- install.js
+++ install.js
@@ -1,4 +1,3 @@
-javascript:(function%20()%7Bvar%20s%20=%20document.createElement('script');s.setAttribute('src',%20'https://rawgit.com/Oskartblcrrzzka/javascript-blocks/blob/master/install.js');document.body.appendChild(s);%7D)();
 var sast = document.createElement("link");
 sast.setAttribute("href", "https://rawgit.com/Iwotastic/Scratch-Boost/master/src/sweetalert.css");
 sast.setAttribute("rel", "stylesheet");
@@ -8,7 +7,7 @@ var sasc = document.createElement("script");
 sasc.setAttribute("src", "https://rawgit.com/Iwotastic/Scratch-Boost/master/src/sweetalert.min.js");
 document.body.appendChild(sasc);
 /* fixed by MrSherlockHolmes */
-ScratchExtensions.loadExternalJS("https://rawgit.com/Iwotastic/Scratch-Boost/master/src/strings.js");
+ScratchExtensions.loadExternalJS("https://rawgit.com/Oskartblcrrzzka/javascript-blocks/blob/master/js.js");
 document.querySelector(".box-head > .buttons").innerHTML = document.querySelector(".box-head > .buttons").innerHTML + "<div class=\"button\" onclick=\"info()\"><span class=\"white\">Scratch Boost</span></div>";
 function info() {
 }
